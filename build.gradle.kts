3f7d011ca98d38992df49e40044589c1d8f74f0e
Andreas Fehn <fehnomenal@fehn.systems>
2020-05-25 12:09:43 +0200

    Create empty java library project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `java-library`
+
+    id("com.olafmertens.git-version") version "0.1.1"
+}
+
+group = "systems.fehn"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "hashids-spring-boot-starter"
