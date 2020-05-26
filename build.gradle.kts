d21dc6922e1cd62f169cef87afb3c7546a45fc68
Andreas Fehn <fehnomenal@fehn.systems>
2020-05-26 22:23:54 +0200

    Allow publishing to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `java-library`
+    `maven-publish`
+    signing
 
     id("org.ajoberstar.grgit") version "4.0.2"
 
@@ -10,6 +12,7 @@ plugins {
 
 group = "systems.fehn"
 version = grgit.describe(mapOf("tags" to true))
+val isRelease = !version.toString().substringAfterLast('-').startsWith('g')
 
 repositories {
     mavenCentral()
@@ -23,4 +26,69 @@ dependencies {
     api("org.hashids:hashids:1.0.3")
 
     compileOnly("com.fasterxml.jackson.core:jackson-databind")
+}
+
+java.withSourcesJar()
+java.withJavadocJar()
+
+tasks.jar {
+    enabled = true
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            version = if (isRelease) project.version.toString() else "${project.version}-SNAPSHOT"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("$group:${project.name}")
+                description.set("Use Hashids with Spring Boot")
+                url.set("https://github.com/fehnomenal/hashids-spring-boot-starter")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("afehn")
+                        name.set("Andreas Fehn")
+                        email.set("fehnomenal@fehn.systems")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/fehnomenal/hashids-spring-boot-starter.git")
+                    developerConnection.set("scm:git:ssh://github.com:fehnomenal/hashids-spring-boot-starter.git")
+                    url.set("https://github.com/fehnomenal/hashids-spring-boot-starter")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = if (isRelease) {
+                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            } else {
+                uri("https://oss.sonatype.org/content/repositories/snapshots")
+            }
+
+            credentials {
+                username = project.property("ossrh.username").toString()
+                password = project.property("ossrh.password").toString()
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
